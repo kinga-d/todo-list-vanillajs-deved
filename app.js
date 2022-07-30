7e1e223ed4c7cc8f842203c18e4e783fe1b8dad0
Kinga Danielewicz <danielewicz.kinga@gmail.com>
2022-07-30 21:32:26 +0200

    styling + buttons

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 //Selectors
-const todoInput = 
-
-
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
+todoButton.addEventListener('click', addTodo);
 
 //Functions
+function addTodo(event) {
+    event.preventDefault(); //prevent from submitting
+
+    //Create todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //Check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //Append to the list
+    todoList.appendChild(todoDiv);
+}
